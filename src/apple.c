02dfcbc6d3abbbcdc50dd1ed7edbe03c9606b04f
Masumi Arakawa <masumi2199@gmail.com>
2019-06-24 18:52:57 +0900

    a#

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i,s;
+  s = 0;
+  for (i = 0; i < n; i++){
+    s += (A[i] + x - 1) / x;
+  }
+  return s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if (p(m) >0) {
+      ub = m;
+    }
+    else {
+      lb = m;
+    }
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,24 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i, s, w;
+  s = 0;
+  w = 0;
+  for(i = 1; i < n; i++){
+    if(A[i] > x){
+      return 0;
+    }
+    else if(w+A[i] > x){
+      s += 1;
+      w = A[i];
+    }
+    else {
+      w += A[i];
+    }
+  }
+return s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +29,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 10000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(p(m) >0) {
+      ub = m;
+    }
+    else {
+      lb = m;
+    }
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
